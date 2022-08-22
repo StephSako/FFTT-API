196a67be6ec11f3e337d4b13c8195ccca0b6cbde
Steph Sako <stephen.sakovitch@orange.fr>
2022-08-22 18:05:29 +0200

    Début tests unitaires - premier test Jest & config

---
+++ source/Tests/Util.spec.ts
@@ -0,0 +1,6 @@
+describe('Util service', () => {
+    test('Demo', () => {
+        const a = 2 + 2
+        expect(a).toBe(4)
+    })
+})
--- source/route/routes.ts
+++ source/route/routes.ts
@@ -1,7 +1,7 @@
 /** source/routes/posts.ts */
-import express from 'express';
-import { ActualiteController } from '../controller/ActualiteController';
-const router = express.Router();
+// import express from 'express';
+// import { ActualiteController } from '../controller/ActualiteController';
+// const router = express.Router();
 
 // router.get('/get/actualites', ActualiteController.log());
 // router.get('/posts/:id', controller.getPost);
@@ -9,4 +9,4 @@ const router = express.Router();
 // router.delete('/posts/:id', controller.deletePost);
 // router.post('/posts', controller.addPost);
 
-export = router;
+// export = router;
